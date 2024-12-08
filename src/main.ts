7e2b4323528ae2e46ef5bec2a248addc81f71d60
mdauwal <pricelezzchap@gmail.com>
2024-12-08 11:53:21 +0000

    swagger ui configure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation') 
+    .setDescription('The API description') 
+    .setVersion('1.0') 
+    .build();
+
+  //Instantiate Document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
